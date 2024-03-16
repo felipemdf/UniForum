b5e3fd4613cfb61145b8f3bef7eee712d7019c66
felipe <f.marinho162001@gmail.com>
2024-03-15 22:46:46 -0300

    refactor: organizado as pastas

---
+++ front-end/vue.config.js
@@ -0,0 +1,13 @@
+const pages = {  
+    index: "src/main.js",  
+  };  
+    
+  // eslint-disable-next-line no-undef
+  module.exports = {  
+    publicPath: "/static/vue/",
+    outputDir: "./build/static/vue/",
+    indexPath: "../../templates/vue_index.html",
+    
+    pages: pages,  
+    
+  };
