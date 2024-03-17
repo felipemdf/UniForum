10b2f6cc2eb5cc37b1cf5a7bd2e7083f5898719f
João Victor <jomiem2014@gmail.com>
2024-03-17 18:16:57 -0400

    Feito modelo de usuario, topico e comentario

--- back-end/apps/account/models.py
+++ back-end/apps/account/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    # class sexEnum(models.TextChoices):
+    #     MASCULINO = "MAS", _("Masculino")
+    #     FEMININO = "FEM", _("Feminino")
+        
+    sexChoices = [
+        "Masculino",
+        "Feminino"
+    ]
+
+    # class courseEnum(models.TextChoices):
+    #     ADMINISTRACAO = "Administração"
+    #     BIOMEDICINA = "Biomedicina"
+    #     ENFERMAGEM = "Enfermagem"
+    #     ENGENHARIA_CIVIL = "Engenharia Civil"
+    #     FARMACIA = "Farmácia"
+    #     NUTRICAO = "Nutrição"
+    #     ARQUITETURA_E_URBANISMO = "Arquitetura e Urbanimos"
+    #     DIREITO = "Direito"
+    #     ENGENHARIA_AMBIENTAL = "Engenharia Ambiental"
+    #     LETRAS  = "Letras"
+    #     PSICOLOGIA = "Psicologia"
+    #     ADS  = "Análise e Desenvolvimento de Sistema"
+    #     CIENCIAS_CONTABEIS = "Ciências Contabeis"
+    #     ENGENHARIA_MECANICA = "Engenharia Mecânica"
+    #     GASTRONOMIA = "Gastronomia"
+    #     PEDAGOGIA = "Pedagogia"
+    #     SISTEMAS_DE_INFORMACAO = "Sistemas de Informção"
+        
+    couserChoices = [
+        "Administração"
+        "Biomedicina"
+        "Enfermagem"
+        "Engenharia Civil"
+        "Farmácia"
+        "Nutrição"
+        "Arquitetura e Urbanimos"
+        "Direito"
+        "Engenharia Ambiental"
+        "Letras"
+        "Psicologia"
+        "Análise e Desenvolvimento de Sistema"
+        "Ciências Contabeis"
+        "Engenharia Mecânica"
+        "Gastronomia"
+        "Pedagogia"
+        "Sistemas de Informção"
+    ]
+
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_leagth=90)
+    matriculation = models.CharField(max_leaght=30,blank = False)
+    email = models.CharField(max_leaght=90, blank = False)
+    password = models.CharField(max_leaght=30, blank = False)
+    course = models.CharField(choices=couserChoices, blank = False)
+    photo = models.ImageField()
+    sex = models.CharField(sexChoices)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- back-end/apps/forum/models.py
+++ back-end/apps/forum/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+class Topic(models.Model):
+
+    # class tagEnum(models.TextChoices):
+    #     DUVIDA = "Duvida" #Essa tag para duvidas
+    #     DIVULGACAO_EST = "Divulgação Estudantil" #Essa tag é para divulgação de trabalhos, artigos, entre outros...
+    #     DIVULGACAO_OP = "Divulgação de Oportunidade" #Essa tag é para divulgação de Estagios, concursos, entre outros...
+        
+    tagChoices = [
+         "Dúvida",
+         "Artigo",
+         "Projeto",
+         "Oportunidade",
+         "Evento"
+    ]
+    
+    couserChoices = [
+        "Administração"
+        "Biomedicina"
+        "Enfermagem"
+        "Engenharia Civil"
+        "Farmácia"
+        "Nutrição"
+        "Arquitetura e Urbanimos"
+        "Direito"
+        "Engenharia Ambiental"
+        "Letras"
+        "Psicologia"
+        "Análise e Desenvolvimento de Sistema"
+        "Ciências Contabeis"
+        "Engenharia Mecânica"
+        "Gastronomia"
+        "Pedagogia"
+        "Sistemas de Informção"
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+    id_author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_leaght=120)
+    content = models.TextField()
+    tag = models.CharField(choices=tagChoices)
+    course = models.CharField(couserChoices)
+    qt_likes = models.IntegerField()
+    qt_comments = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+    
+
+class Comments(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    id_topic = models.ForeignKey(Topic ,on_delete=models.CASCADE)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    qt_likes = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
